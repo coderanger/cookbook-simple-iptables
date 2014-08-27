035b32d09d38cdf39010cecc337d1ae05b1ab1b8
Noah Kantrowitz <noah@coderanger.net>
2014-08-26 17:36:02 -0700

    Trying to fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,5 @@
-default["simple_iptables"]["rules"] = {"filter" => [], "nat" => [], "mangle" => [], "raw" => []}
+set["simple_iptables"]["rules"] = {"filter" => [], "nat" => [], "mangle" => [], "raw" => []}
 default["simple_iptables"]["chains"] = {"filter" => [], "nat" => [], "mangle" => [], "raw" => []}
 default["simple_iptables"]["policy"] = {"filter" => {}, "nat" => {}, "mangle" => {}, "raw" => {}}
 
 default["simple_iptables"]["tables"] = %w(filter nat mangle raw)
-
-default["simple_iptables"]["rules"]['filter'] = []
