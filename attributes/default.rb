9c7ebf8317fdc7f0258b24e0b5195475f6773006
Noah Kantrowitz <noah@coderanger.net>
2014-08-26 17:32:13 -0700

    Stuff

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default["simple_iptables"]["chains"] = {"filter" => [], "nat" => [], "mangle" =>
 default["simple_iptables"]["policy"] = {"filter" => {}, "nat" => {}, "mangle" => {}, "raw" => {}}
 
 default["simple_iptables"]["tables"] = %w(filter nat mangle raw)
+
+default["simple_iptables"]["rules"]['filter'] = []
